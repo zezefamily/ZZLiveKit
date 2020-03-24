3bdea33d72535b500ad5a41ad37aceeff3eb0779
wuwenmei <wuwenmei@100tal.com>
2020-03-25 00:34:20 +0800

    新增一些代码

--- ZZLiveKit/ZZLiveKit/Tools/ZZVideoCodecTool.h
+++ ZZLiveKit/ZZLiveKit/Tools/ZZVideoCodecTool.h
@@ -7,11 +7,67 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <VideoToolbox/VideoToolbox.h>
+
+typedef NS_ENUM(NSUInteger,ZZVideoEncoderProfileLevel) {
+    /*基本画质。支持I/P 帧，只支持无交错（Progressive）和CAVLC；主要应用：可视电话，会议电视，和无线通讯等实时视频通讯领域*/
+    ZZVideoEncoderProfileLevelBP,
+    /*进阶画质。支持I/P/B/SP/SI 帧，只支持无交错（Progressive）和CAVLC*/
+    ZZVideoEncoderProfileLevelEP,
+    /*主流画质。提供I/P/B 帧，支持无交错（Progressive）和交错（Interlaced），也支持CAVLC 和CABAC 的支持；主要应用：数字广播电视和数字视频存储*/
+    ZZVideoEncoderProfileLevelMP,
+    /*高级画质。在main Profile 的基础上增加了8×8内部预测、自定义量化、 无损视频编码和更多的YUV 格式；应用于广电和存储领域*/
+    ZZVideoEncoderProfileLevelHP
+};
+
 
 NS_ASSUME_NONNULL_BEGIN
+@interface ZZVideoEncoderParam : NSObject
+//设置编码的画质，默认：BP
+@property (nonatomic,assign) ZZVideoEncoderProfileLevel profileLevel;
+//编码内容的宽度
+@property (nonatomic,assign) NSInteger encodeWidth;
+//编码内容的高度
+@property (nonatomic,assign) NSInteger encodeHeight;
+//编码类型
+@property (nonatomic,assign) CMVideoCodecType encodeType;
+//码率 kbps
+@property (nonatomic,assign) NSInteger bitRate;
+//帧率 fps 默认15bps
+@property (nonatomic,assign) NSInteger frameRate;
+//最大I帧间隔 s 默认240s一个帧
+@property (nonatomic,assign) NSInteger maxKeyFrameInterval;
+//是否允许产生B帧 默认NO
+@property (nonatomic,assign) BOOL allowFrameReordering;
+@end
+
+
+@protocol ZZVideoCodecToolDelegate <NSObject>
+
+/// 编码输出数据
+/// @param data 数据的二进制数据
+/// @param isKeyFrame 是否为关键帧
+- (void)videoEncodeOutputDataCallBack:(NSData *)data isKeyFrame:(BOOL)isKeyFrame;
+
+@end
 
 @interface ZZVideoCodecTool : NSObject
 
+@property (nonatomic,weak) id<ZZVideoCodecToolDelegate> delegate;
+
+@property (nonatomic,strong) ZZVideoEncoderParam *videoEncodeParam;
+
+- (instancetype)initWithParam:(ZZVideoEncoderParam *)param;
+
+- (BOOL)startVideoEncode;
+
+- (BOOL)stopVideoEncode;
+
+- (BOOL)videoEncodeInputSampleBuffer:(CMSampleBufferRef)sampleBuffer isForceKeyFrame:(BOOL)forceKeyFrame;
+
+- (BOOL)changeBitRate:(NSInteger)bitRate;
+
 @end
+
 
 NS_ASSUME_NONNULL_END
--- ZZLiveKit/ZZLiveKit/Tools/ZZVideoCodecTool.m
+++ ZZLiveKit/ZZLiveKit/Tools/ZZVideoCodecTool.m
@@ -14,34 +14,69 @@
 
 #import "ZZVideoCodecTool.h"
 #import <VideoToolbox/VideoToolbox.h>
-@interface ZZVideoCodecTool ()
-{
-    OSStatus _status;
-    VTCompressionOutputCallback _outputCallback;
-    VTCompressionSessionRef _sessionRef;
-}
-@end
-@implementation ZZVideoCodecTool
+
+@implementation ZZVideoEncoderParam
 - (instancetype)init
 {
     if(self == [super init]){
+        self.profileLevel = ZZVideoEncoderProfileLevelBP;
+        self.encodeType = kCMVideoCodecType_H264;
+        self.bitRate = 1024 * 1024;
+        self.frameRate = 15;
+        self.maxKeyFrameInterval = 240;
+        self.allowFrameReordering = NO;
+    }
+    return self;
+}
+@end
+
+
+@interface ZZVideoCodecTool ()
+{
+    
+}
+@property (nonatomic,assign) VTCompressionSessionRef sessionRef;
+
+@end
+@implementation ZZVideoCodecTool
+- (instancetype)initWithParam:(ZZVideoEncoderParam *)param
+{
+    if(self == [super init]){
+        self.videoEncodeParam = param;
         [self instanceSession];
     }
     return self;
 }
+
+
 - (void)instanceSession
 {
     //1.创建编码器
-    _status = VTCompressionSessionCreate(NULL, 640, 320, kCMVideoCodecType_H264, NULL, NULL, NULL, _outputCallback, (__bridge void * _Nullable)(self), &(_sessionRef));
+    OSStatus status = VTCompressionSessionCreate(NULL, 640, 320, kCMVideoCodecType_H264, NULL, NULL, NULL, outputCallback, (__bridge void * _Nullable)(self), &(_sessionRef));
+    if(noErr != status){
+        NSLog(@"VTCompressionSessionCreate failed:%d",(int)status);
+        return;
+    }
+    if(NULL == self.sessionRef){
+        NSLog(@"ZZVideoCodecTool::调用顺序错误");
+        return;
+    }
     //2.设置编码器属性 准备编码
     // 设置码率 512kbps
-    OSStatus status = VTSessionSetProperty(_sessionRef, kVTCompressionPropertyKey_AverageBitRate, (__bridge CFTypeRef)@(512 * 1024));
-    // 设置ProfileLevel为BP3.1
+    status = VTSessionSetProperty(_sessionRef, kVTCompressionPropertyKey_AverageBitRate, (__bridge CFTypeRef)@(512 * 1024));
+    // 设置画质 ProfileLevel为BP3.1
+    /**
+        实时直播：
+        低清Baseline Level 1.3
+        标清Baseline Level 3
+        半高清Baseline Level 3.1
+        全高清Baseline Level 4.1
+    */
     status = VTSessionSetProperty(_sessionRef, kVTCompressionPropertyKey_ProfileLevel, kVTProfileLevel_H264_Baseline_3_1);
     // 设置实时编码输出（避免延迟）
     status = VTSessionSetProperty(_sessionRef, kVTCompressionPropertyKey_RealTime, kCFBooleanTrue);
     // 配置是否产生B帧 kCFBooleanFalse
-    status = VTSessionSetProperty(_sessionRef, kVTCompressionPropertyKey_AllowFrameReordering, kCFBooleanTrue);
+    status = VTSessionSetProperty(_sessionRef, kVTCompressionPropertyKey_AllowFrameReordering, kCFBooleanFalse);
     // 配置最大I帧间隔  15帧 x 240秒 = 3600帧，也就是每隔3600帧编一个I帧
     status = VTSessionSetProperty(_sessionRef, kVTCompressionPropertyKey_MaxKeyFrameInterval, (__bridge CFTypeRef)@(15 * 240));
     // 配置I帧持续时间，240秒编一个I帧
@@ -50,22 +85,160 @@
     status = VTCompressionSessionPrepareToEncodeFrames(_sessionRef);
 }
 
-- (void)startEcodeFrameWithSampleBuffer:(CMSampleBufferRef)sampleBufferRef
+- (BOOL)startVideoEncode
 {
-    //CMSampleBufferRef --> CVImageBufferRef
-    CVImageBufferRef imageBuffer = (CVImageBufferRef)CMSampleBufferGetImageBuffer(sampleBufferRef);
-    NSDictionary *frameProperties = @{(__bridge NSString *)kVTEncodeFrameOptionKey_ForceKeyFrame:@(0)};
-    VTCompressionSessionEncodeFrame(_sessionRef, imageBuffer, kCMTimeInvalid, kCMTimeInvalid, (__bridge CFDictionaryRef)frameProperties, NULL, NULL);
+    if(NULL == _sessionRef){
+        NSLog(@"ZZVideoCodecTool::调用顺序错误");
+        return NO;
+    }
+    //编码器准备编码
+    OSStatus status = VTCompressionSessionPrepareToEncodeFrames(_sessionRef);
+    if(noErr != status){
+        NSLog(@"ZZVideoCodecTool:VTCompressionSessionPrepareToEncodeFrames failed status:%d", (int)status);
+        return NO;
+    }
+    return YES;
 }
 
-- (void)stopEcodeFrame
+- (BOOL)stopVideoEncode
 {
+    if(NULL != _sessionRef){
+        return NO;
+    }
     //停止编码
+    OSStatus status = VTCompressionSessionCompleteFrames(_sessionRef, kCMTimeInvalid);
+    if(noErr != status){
+        NSLog(@"ZZVideoCodecTool::VTCompressionSessionCompleteFrames failed! status:%d", (int)status);
+        return NO;
+    }
+    return YES;
+}
+
+- (BOOL)videoEncodeInputSampleBuffer:(CMSampleBufferRef)sampleBuffer isForceKeyFrame:(BOOL)forceKeyFrame
+{
+    //CMSampleBufferRef --> CVImageBufferRef
+    CVImageBufferRef imageBuffer = (CVImageBufferRef)CMSampleBufferGetImageBuffer(sampleBuffer);
+    NSDictionary *frameProperties = @{(__bridge NSString *)kVTEncodeFrameOptionKey_ForceKeyFrame:@(0)};
+    VTCompressionSessionEncodeFrame(_sessionRef, imageBuffer, kCMTimeInvalid, kCMTimeInvalid, (__bridge CFDictionaryRef)frameProperties, NULL, NULL);
+    return YES;
+}
+
+#pragma mark - 编码回调结果???????
+void outputCallback(void * CM_NULLABLE outputCallbackRefCon,void * CM_NULLABLE sourceFrameRefCon,OSStatus status,VTEncodeInfoFlags infoFlags,CM_NULLABLE CMSampleBufferRef sampleBuffer ){
+    if (noErr != status || nil == sampleBuffer)
+    {
+        NSLog(@"VEVideoEncoder::encodeOutputCallback Error : %d!", (int)status);
+        return;
+    }
+    if (nil == outputCallbackRefCon)
+    {
+        return;
+    }
+    if (!CMSampleBufferDataIsReady(sampleBuffer))
+    {
+        return;
+    }
+    if (infoFlags & kVTEncodeInfo_FrameDropped)
+    {
+        NSLog(@"VEVideoEncoder::H264 encode dropped frame.");
+        return;
+    }
+    ZZVideoCodecTool *encoder = (__bridge ZZVideoCodecTool *)outputCallbackRefCon;
+    const char header[] = "\x00\x00\x00\x01";
+    size_t headerLen = (sizeof header) - 1;
+    NSData *headerData = [NSData dataWithBytes:header length:headerLen];
+    
+    // 判断是否是关键帧
+    bool isKeyFrame = !CFDictionaryContainsKey((CFDictionaryRef)CFArrayGetValueAtIndex(CMSampleBufferGetSampleAttachmentsArray(sampleBuffer, true), 0), (const void *)kCMSampleAttachmentKey_NotSync);
+    
+    if (isKeyFrame)
+    {
+        NSLog(@"VEVideoEncoder::编码了一个关键帧");
+        CMFormatDescriptionRef formatDescriptionRef = CMSampleBufferGetFormatDescription(sampleBuffer);
+        
+        // 关键帧需要加上SPS、PPS信息
+        size_t sParameterSetSize, sParameterSetCount;
+        const uint8_t *sParameterSet;
+        OSStatus spsStatus = CMVideoFormatDescriptionGetH264ParameterSetAtIndex(formatDescriptionRef, 0, &sParameterSet, &sParameterSetSize, &sParameterSetCount, 0);
+        
+        size_t pParameterSetSize, pParameterSetCount;
+        const uint8_t *pParameterSet;
+        OSStatus ppsStatus = CMVideoFormatDescriptionGetH264ParameterSetAtIndex(formatDescriptionRef, 1, &pParameterSet, &pParameterSetSize, &pParameterSetCount, 0);
+        
+        if (noErr == spsStatus && noErr == ppsStatus)
+        {
+            NSData *sps = [NSData dataWithBytes:sParameterSet length:sParameterSetSize];
+            NSData *pps = [NSData dataWithBytes:pParameterSet length:pParameterSetSize];
+            NSMutableData *spsData = [NSMutableData data];
+            [spsData appendData:headerData];
+            [spsData appendData:sps];
+            if ([encoder.delegate respondsToSelector:@selector(videoEncodeOutputDataCallBack:isKeyFrame:)])
+            {
+                [encoder.delegate videoEncodeOutputDataCallBack:spsData isKeyFrame:isKeyFrame];
+            }
+            
+            NSMutableData *ppsData = [NSMutableData data];
+            [ppsData appendData:headerData];
+            [ppsData appendData:pps];
+            
+            if ([encoder.delegate respondsToSelector:@selector(videoEncodeOutputDataCallBack:isKeyFrame:)])
+            {
+                [encoder.delegate videoEncodeOutputDataCallBack:ppsData isKeyFrame:isKeyFrame];
+            }
+        }
+    }
+    
+    CMBlockBufferRef blockBuffer = CMSampleBufferGetDataBuffer(sampleBuffer);
+    size_t length, totalLength;
+    char *dataPointer;
+    status = CMBlockBufferGetDataPointer(blockBuffer, 0, &length, &totalLength, &dataPointer);
+    if (noErr != status)
+    {
+        NSLog(@"VEVideoEncoder::CMBlockBufferGetDataPointer Error : %d!", (int)status);
+        return;
+    }
+    
+    size_t bufferOffset = 0;
+    static const int avcHeaderLength = 4;
+    while (bufferOffset < totalLength - avcHeaderLength)
+    {
+        // 读取 NAL 单元长度
+        uint32_t nalUnitLength = 0;
+        memcpy(&nalUnitLength, dataPointer + bufferOffset, avcHeaderLength);
+        
+        // 大端转小端
+        nalUnitLength = CFSwapInt32BigToHost(nalUnitLength);
+        
+        NSData *frameData = [[NSData alloc] initWithBytes:(dataPointer + bufferOffset + avcHeaderLength) length:nalUnitLength];
+        
+        NSMutableData *outputFrameData = [NSMutableData data];
+        [outputFrameData appendData:headerData];
+        [outputFrameData appendData:frameData];
+        
+        bufferOffset += avcHeaderLength + nalUnitLength;
+        
+        if ([encoder.delegate respondsToSelector:@selector(videoEncodeOutputDataCallBack:isKeyFrame:)])
+        {
+            [encoder.delegate videoEncodeOutputDataCallBack:outputFrameData isKeyFrame:isKeyFrame];
+        }
+    }
+}
+
+
+- (BOOL)changeBitRate:(NSInteger)bitRate
+{
+    return YES;
+}
+
+- (void)dealloc
+{
+    if(NULL != _sessionRef){
+        return;
+    }
     VTCompressionSessionCompleteFrames(_sessionRef, kCMTimeInvalid);
     //释放编码器
     VTCompressionSessionInvalidate(_sessionRef);
     CFRelease(_sessionRef);
     _sessionRef = NULL;
 }
-
 @end
